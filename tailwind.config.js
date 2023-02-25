30e5f28ae480f0e1e9e07d4563771e894aec5c9d
lilisako <lilisako062397@gmail.com>
2023-02-25 13:28:36 +0900

    fix: add styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [],
+  theme: {
+    extend: {
+        fontFamily: {
+            'vt': ['VT323', 'cursive'],
+        },
+    },
+},
+  plugins: [],
+}
